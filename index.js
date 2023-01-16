c3db02757816f51c33da0005d9ba5158d18153ca
Bilal Shaikh <bilalwaaiz@gmail.com>
2023-01-16 09:09:11 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const calculate_score_btn = document.querySelector("#submit")
+const to_hide_div = document.querySelector("#tohide")
+const to_show_div = document.querySelector("#toshow")
+const reset_btn = document.querySelector("#reset")
+const clear_btn = document.querySelector("#clear")
+
+to_hide_div.style.display = "block"
+to_show_div.style.display = "none"
+
+calculate_score_btn.addEventListener("click", () => {
+    let purity = 100 - document.querySelectorAll('input[type="checkbox"]:checked').length
+    document.querySelector("#score").innerHTML = purity
+    to_hide_div.style.display = "none"
+    to_show_div.style.display = "block"
+})
+
+reset_btn.addEventListener("click", () => {
+    to_hide_div.style.display = "block"
+    to_show_div.style.display = "none"
+    document.querySelectorAll('input[type="checkbox"]').forEach( (element) => {
+        element.checked = false
+    })
+})
+
+clear_btn.addEventListener("click", () => {
+    document.querySelectorAll('input[type="checkbox"]').forEach( (element) => {
+        element.checked = false
+    })
+})
